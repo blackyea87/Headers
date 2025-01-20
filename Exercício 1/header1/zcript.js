4c0f37d12329e4d9f0b7bfeba648f827c98ed175
blackyea <156470135+blackyea87@users.noreply.github.com>
2025-01-20 19:44:04 -0300

    Add files via upload

---
+++ Exercício 1/header1/zcript.js
@@ -0,0 +1,37 @@
+/*
+Conexão Terminada.
+
+Sinto interromper você, Elizabeth, se você ainda se lembra desse nome, mas temo que 
+tenha sido mal informada. Você não está aqui para receber um presente, nem foi chamada 
+pela pessoa que você presume, embora, de fato, tenha sido chamada. Vocês todos foram 
+chamados aqui, para um labirinto de sons e cheiros, desvios e infortúnios. Um labirinto 
+sem saída, um enigma sem prêmio. Vocês nem percebem que estão presos. A sua sede de 
+sangue os levou em círculos sem fim, caçando os gritos de crianças em alguma câmara 
+invisível, sempre parecendo tão perto, mas de alguma forma fora de alcance, mas vocês 
+nunca as encontrarão. Nenhum de vocês encontrará. É aqui que a sua história termina.
+
+E para você, meu bravo voluntário, que de alguma forma encontrou esta vaga de emprego 
+não destinada a você, embora houvesse uma saída planejada para você, tenho a sensação 
+de que não é isso que você quer. Tenho a sensação de que você está exatamente onde quer 
+estar. Eu também estou permanecendo. Estou por perto. Este lugar não será lembrado, e a 
+memória de tudo que começou isso finalmente poderá começar a desaparecer. Como a agonia 
+de toda tragédia deveria.
+
+E para vocês, monstros presos nos corredores, fiquem quietos e renunciem aos seus 
+espíritos. Eles não pertencem a vocês. Para a maioria de vocês, acredito que haja paz e 
+talvez mais esperando por vocês depois que a fumaça se dissipar. Embora, para um de 
+vocês, o poço mais escuro do Inferno se abriu para engoli-lo inteiro, então não faça o 
+diabo esperar, velho amigo.
+
+Minha filha, se você puder me ouvir, eu sabia que você também retornaria. Está na sua 
+natureza proteger os inocentes. Sinto muito que naquele dia, o dia em que você foi 
+fechada e deixada para morrer, ninguém estava lá para erguê-la em seus braços como você 
+ergueu outros em seus braços, e então, o que aconteceu com você. Eu deveria ter sabido 
+que você não se contentaria em desaparecer, não minha filha. Eu não pude salvá-la então, 
+então deixe-me salvá-la agora. É hora de descansar - para você, e para aqueles que você 
+carregou em seus braços.
+
+Isto termina para todos nós.
+
+Fim da comunicação.
+*/
